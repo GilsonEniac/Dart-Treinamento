f13eeb465cdb01f65f7a43115199f736f53a7fdc
GilsonEniac <gilson.eniac@gmail.com>
2023-02-16 18:17:15 -0300

    Create yesy.dart

---
+++ 19-Coleção LinkedHasMap/yesy.dart
@@ -0,0 +1,23 @@
+main() {
+//Com objetos
+
+  List<Map<String, Object?>> escola = [
+    {
+      'nome': 'Turma T1',
+      'alunos': [
+        {'nome': 'Fernando', 'nota': 8.1},
+        {'nome': 'Leila', 'nota': 9.3}
+      ]
+    },
+    {
+      'nome': 'Turma T2',
+      'alunos': [
+        {'nome': 'Theo', 'nota': 8.9},
+        {'nome': 'Bartolomeu', 'nota': 7.3}
+      ]
+    }
+  ];
+
+  var listaAluno =
+  
+}
